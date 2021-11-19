b1da568146932b12c0eceda86aa6e6034478c250
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2021-11-19 20:13:01 +0000

    Exercicio 1

--- Program.cs
+++ Program.cs
@@ -4,9 +4,22 @@ namespace dotnetcore
 {
     class Program
     {
+        // Exercício 1:
+        // - Fazer a média de notas dos dois alunos (Zuqui, Bronza) e imprimir no Console.WriteLine();
+        // - Imprimir no console para cada aluno
+        //      - Nome do aluno
+        //      - Caso média maior ou igual 7:
+        //          - Passou no ano letivo
+        //          - Não passou no ano letivo
+        // Requisítos:
+        // - Criar um método "CalcularMedia", onde a entrada de parâmetros seja as notas e o retorno seja a média em float
+        // - Utilizar For ou Foreach
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Iniciando Programa");
+
+            var xNotasZuqui = new int[] { 7, 6, 5, 3 };
+            var xNotasBronza = new int[] { 8, 9, 10, 8 };
         }
     }
 }
